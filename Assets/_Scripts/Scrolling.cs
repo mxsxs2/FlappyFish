5f560b2449a35c439419d440a16c5d3ec44849ba
Mxsxs2 <>
2018-10-10 19:49:08 +0100

    Add scrolling movement to seaweed

---
+++ Assets/_Scripts/Scrolling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Scrolling : MonoBehaviour {
+
+    [SerializeField]
+    //Falling speed
+    public float speed = 2f;
+    //The object that falls    
+    private Rigidbody2D rb;
+    void Start()
+    {
+        //Get the falling object
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        //Make the object go left on every frame
+        rb.velocity = Vector2.left * speed;
+
+    }
+}
